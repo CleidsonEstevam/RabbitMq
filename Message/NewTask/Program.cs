2c3650ca18b1a97276728b7b58e6bbc7a4ed9337
Cleidson Estevam Da Silva <cleidson.silva@reval.corp>
2023-03-17 13:45:02 -0300

    BasicQos

--- Message/NewTask/Program.cs
+++ Message/NewTask/Program.cs
@@ -1,12 +1,13 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using RabbitMQ.Client;
 
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "hello",
-                     durable: false,
+channel.QueueDeclare(queue: "task_queue",
+                     durable: true,
                      exclusive: false,
                      autoDelete: false,
                      arguments: null);
@@ -14,12 +15,13 @@ channel.QueueDeclare(queue: "hello",
 var message = GetMessage(args);
 var body = Encoding.UTF8.GetBytes(message);
 
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 channel.BasicPublish(exchange: string.Empty,
-                     routingKey: "hello",
-                     basicProperties: null,
+                     routingKey: "task_queue",
+                     basicProperties: properties,
                      body: body);
-
-
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
@@ -27,5 +29,5 @@ Console.ReadLine();
 
 static string GetMessage(string[] args)
 {
-    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    return ((args.Length > 0) ? string.Join(" ", args) : "Falaaaaaaaaaaaaaaaaaaaa");
 }
